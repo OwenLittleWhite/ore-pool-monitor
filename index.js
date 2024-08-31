286b3c5a3cc5fbb1397f093e2361744f2a3481a5
owen <253065903@qq.com>
2024-09-01 00:13:10 +0800

    feat: fix

--- index.js
+++ index.js
@@ -7,13 +7,19 @@ const connection = new Connection("https://mainnet.helius-rpc.com/?api-key=5380e
 
 // 监控的钱包地址及其对应的执行脚本路径
 const walletsToMonitor = {
-    "2efeLemAKwpnWjs3vdkAcYTaH6RVmXLeULtNp4JScHjR": "/root/ore-hq-server/server1/restart1.sh",
-    "2CmzZ7yq2JqY8aeaNiPidbVVrbcbwT2Aj8a7q45scNdL": "/root/ore-hq-server/server2/restart2.sh",
+    "2efeLemAKwpnWjs3vdkAcYTaH6RVmXLeULtNp4JScHjR": {
+        directory: "/root/ore-hq-server/server1",
+        scriptPath: "restart1.sh"
+    },
+    "2CmzZ7yq2JqY8aeaNiPidbVVrbcbwT2Aj8a7q45scNdL": {
+        directory: "/root/ore-hq-server/server2",
+        scriptPath: "restart2.sh"
+    }
     // 添加更多的地址和脚本
 };
 
 // 检查Solana钱包的余额和最后交易时间
-async function checkBalanceAndLastTx(walletAddress, scriptPath) {
+async function checkBalanceAndLastTx(walletAddress, scriptPath, directory) {
     try {
         const fiveMinutesAgo = Date.now() - (5 * 60 * 1000); // 五分钟后的时间戳
 
@@ -28,15 +34,13 @@ async function checkBalanceAndLastTx(walletAddress, scriptPath) {
             commitment: "confirmed",
             limit: 1
         });
-        console.log(JSON.stringify(signatures), "===")
-
         const lastTxTime = signatures.length > 0 ? new Date(signatures[0].blockTime*1000) : null;
 
         console.log(`Balance: ${balanceInSol} SOL, Last Transaction Time: ${lastTxTime ? lastTxTime.toISOString() : 'No transactions found'} for address ${walletAddress}`);
 
         if (balanceInSol < 0.001 || (!lastTxTime || lastTxTime.getTime() < fiveMinutesAgo)) {
             console.log(`Balance is less than 0.01 SOL or last transaction was more than 5 minutes ago for address ${walletAddress}.`);
-            executeScript(scriptPath);
+            executeScript(scriptPath, directory);
         } else {
             console.log(`Balance: ${balanceInSol} SOL, Last Transaction Time: ${lastTxTime ? lastTxTime.toISOString() : 'No transactions found'} for address ${walletAddress}`);
         }
@@ -46,8 +50,12 @@ async function checkBalanceAndLastTx(walletAddress, scriptPath) {
 }
 
 // 执行特定脚本
-function executeScript(scriptPath) {
-    console.log(`Executing script at ${scriptPath}`);
+function executeScript(scriptPath, directory) {
+    console.log(`Executing script at ${path.join(directory, scriptPath)}`);
+
+    // 切换工作目录
+    process.chdir(directory);
+
     exec(`sh ${scriptPath}`, (error, stdout, stderr) => {
         if (error) {
             console.error(`Error executing script (${scriptPath}): ${error}`);
@@ -61,16 +69,17 @@ function executeScript(scriptPath) {
     });
 }
 
+
 // 设置定时器每五分钟执行一次
 setInterval(() => {
     Object.keys(walletsToMonitor).forEach(async (address) => {
-        const scriptPath = walletsToMonitor[address];
-        await checkBalanceAndLastTx(address, scriptPath);
+        const { scriptPath, directory } = walletsToMonitor[address];
+        await checkBalanceAndLastTx(address, scriptPath, directory);
     });
 }, 5 * 60 * 1000);
 
 // 立即执行一次
 Object.keys(walletsToMonitor).forEach(async (address) => {
-    const scriptPath = walletsToMonitor[address];
-    await checkBalanceAndLastTx(address, scriptPath);
+    const { scriptPath, directory } = walletsToMonitor[address];
+        await checkBalanceAndLastTx(address, scriptPath, directory);
 });
